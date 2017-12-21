b3eb12327012f9a8ee1e8555a82be0c91a6686e8
gzuhlwang <2277581700@qq.com>
2017-12-21 14:58:50 +0800

    some optimizations

--- sort/bubble_sort/bubble_sort.go
+++ sort/bubble_sort/bubble_sort.go
@@ -12,11 +12,18 @@ func Bubble_sort(des [10]int, nums int) [10]int {
 	//难点在于确定边界（i,j）
 	for i := 0; i < length; i++ { //nums个数只需要排nums-1趟
 
+		flag := false //表示本趟冒泡是否发生交换的标志
+
+		//一趟冒泡过程
 		for j := 0; j < length-i; j++ {
-			if des[j] < des[j+1] { //按从大到小的顺序排序
-				temp := des[j]
-				des[j] = des[j+1]
-				des[j+1] = temp
+			//按从大到小的顺序排序
+			if des[j] < des[j+1] {
+				des[j], des[j+1] = des[j+1], des[j] //交换
+				flag = true
+			}
+
+			if flag == false {
+				break //本趟遍历后没有发生交换，说明待排序列已经有序
 			}
 		}
 	}
